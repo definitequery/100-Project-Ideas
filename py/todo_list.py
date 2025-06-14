4a7de943f13c133adf46235c660042511c2398f4
Aaron Simpson <aaron@aaronsimpson.org>
2025-06-13 21:59:39 -0500

    Add better error-handling.

--- py/todo_list.py
+++ py/todo_list.py
@@ -22,9 +22,12 @@ def main():
 
 def add_task():
   user_input = input('Enter task: ')
-  task = f"{user_input}"
-  tasks.append(task)
-  print('\nTask added!\n')
+  if user_input:
+    task = f"{user_input}"
+    tasks.append(task)
+    print('\nTask added!\n')
+  else:
+    print('Task cannot be empty. Please enter a task.\n')
 
 def show_tasks():
   print('Your tasks:')
@@ -33,9 +36,15 @@ def show_tasks():
   print()
 
 def remove_task():
-  task_number = int(input('Enter task number to remove: '))
-  tasks.remove(tasks[task_number - 1])
-  print('Task removed!\n')
+  try:
+    task_number = int(input('Enter task number to remove: '))
+    if task_number > 0 and task_number <= len(tasks):
+      tasks.remove(tasks[task_number - 1])
+      print('Task removed!\n')
+    else:
+      print('Task not found. Please enter a valid task number.\n')
+  except ValueError:
+    print('Invalid input. Please enter a valid task number.\n')
 
 if __name__ == "__main__":
   try:
